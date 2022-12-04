cf4aaf9d20a8c5a2bb107ec81ff699a1567dbeb3
Eragoo <y.kukhol@spd-ukraine.com>
2022-12-04 15:59:11 +0200

    Changed get-token mapping from POST to GET

--- src/main/java/com/Eragoo/todo/auth/basic/BasicAuthResource.java
+++ src/main/java/com/Eragoo/todo/auth/basic/BasicAuthResource.java
@@ -1,10 +1,7 @@
 package com.Eragoo.todo.auth.basic;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth/basic")
@@ -13,7 +10,7 @@ public class BasicAuthResource {
     private BasicAuthService service;
 
 
-    @PostMapping("/token")
+    @GetMapping("/token")
     public String getJwtTokenByUsernamePassword(@RequestBody UserAuthInputDto command) {
         return service.getToken(command);
     }
